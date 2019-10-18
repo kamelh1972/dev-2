b1b7e650a58a2f4b7bc66830775908cc97e739e2
hammiche <kamelhammiche44@gmail.com>
2019-10-18 10:26:53 +0200

    add exo1

---
+++ exo.py
@@ -0,0 +1,13 @@
+###exo1
+a = 19
+b = 89
+c = 38
+d = 40
+if a > b and a > c and a > d :
+    print(a)
+elif b > a and b > c and b > d :
+    print(b)
+elif c > a and c > b and c > d :
+    print(c)
+else :
+    print(d)
