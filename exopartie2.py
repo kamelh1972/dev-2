febc2bd5e186dd06439e4fc58438c833cc1f0774
hammiche <kamelhammiche44@gmail.com>
2019-10-23 13:30:48 +0200

    add exercice1 et 2

---
+++ exopartie2.py
@@ -0,0 +1,29 @@
+###exercice1 echiquier
+noire = "# # # # # # # #"
+blanche = ""
+print("")
+print(" \"")
+for i in range(0, 8):
+    if i%2 == 0:
+        print("   {}{}".format(noire, blanche))
+    else:
+        print("    {}{}".format(blanche, noire))
+print(" \"")
+print("")
+
+
+###exercice2
+liste = [0,0,0,0]
+a = 0
+b = 0
+c = 0
+d = 0
+e = "----------"
+for i in range(0, 4):
+    liste[i] = 1
+    a = liste[0]
+    b = liste[1]
+    c = liste[2]
+    d = liste[3]
+    print("{}\n{}\n{}\n{}\n{}".format(a, b, c, d, e))
+    liste[i] = 0
